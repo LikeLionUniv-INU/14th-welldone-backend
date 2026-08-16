4ed28d1360d6d07aeb3237373f25479654476682
jeon1105 <mjeon8785@gmail.com>
2026-08-16 19:00:40 +0900

    초기 프로젝트 세팅

---
+++ src/test/java/com/likelion/welldone/WelldoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.welldone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelldoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
